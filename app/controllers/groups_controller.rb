b6d65d7ab89f4c5a8c9bba96cf8846659980a54a
Luis Saavedra <nriqu322@gmail.com>
2020-05-14 09:40:15 -0600

    add alphabetic order to groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,7 +5,7 @@ class GroupsController < ApplicationController
   # GET /groups
   # GET /groups.json
   def index
-    @groups = Group.all
+    @groups = Group.all.order(:name)
   end
 
   # GET /groups/1
