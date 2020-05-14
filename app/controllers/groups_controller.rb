524826cfcc5f1d169163483b7e2af4fb43c54bff
Luis Saavedra <nriqu322@gmail.com>
2020-05-13 19:30:43 -0600

    add user id to group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,7 +15,7 @@ class GroupsController < ApplicationController
 
   # GET /groups/new
   def new
-    @group = Group.new
+    @group = current_user.groups.build
   end
 
   # GET /groups/1/edit
@@ -25,7 +25,7 @@ class GroupsController < ApplicationController
   # POST /groups
   # POST /groups.json
   def create
-    @group = Group.new(group_params)
+    @group = current_user.groups.build(group_params)
 
     respond_to do |format|
       if @group.save
