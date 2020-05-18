0cbe4a5f28981420f23fa1e6a30bc6841031bfde
Luis Saavedra <nriqu322@gmail.com>
2020-05-18 10:36:09 -0600

    add flash notifications

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -31,7 +31,7 @@ class GroupsController < ApplicationController
 
     respond_to do |format|
       if @group.save
-        format.html { redirect_to @group, notice: 'Group was successfully created.' }
+        format.html { redirect_to groups_path, notice: 'Group was successfully created.' }
         format.json { render :show, status: :created, location: @group }
       else
         format.html { render :new }
