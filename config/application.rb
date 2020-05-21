668832d3b0d1bc62c69904bff93a9aa0b83f3f2a
Luis Saavedra <nriqu322@gmail.com>
2020-05-21 16:32:57 -0600

    avoid precompile

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.assets.initialize_on_precompile = false
 
 module TimeTracker
   class Application < Rails::Application
